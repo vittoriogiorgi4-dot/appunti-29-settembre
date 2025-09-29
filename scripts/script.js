59be8f56217f6edf0aa9998b58f8847af13e349d
vittorio giorgi <vittorio.giorgi4@gmail.com>
2025-09-29 12:45:44 +0200

    add

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//primo file JS
+// questa è una variabile
+let casa
+// questo è il valore della variabile
+casa = "cucina, salotto, bagno"
+// il console.log fa apparire messaggi nel broswer
+console.log(casa)
